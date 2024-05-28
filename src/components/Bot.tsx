083dc4081d9dfb26309c3d35eb4d5d242490c3a1
Alex Oliveira <alex@c3dweb.com.br>
2024-05-28 18:19:18 -0300

    remove marca

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1059,7 +1059,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               />
             )}
           </div>
-
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
